6e8b8f57a22b500a44652a5e3ae8917f5f00ec74
SAHearn1 <Hearn.sa@gmail.com>
2025-08-08 10:34:10 -0400

    chore: transpile firebase, undici, and @firebase/auth via next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 // next.config.js
+const withTM = require('next-transpile-modules')([
+  'firebase',
+  'undici',
+  '@firebase/auth',
+]);
 const path = require('path');
 
-module.exports = {
-  // Tell Next.js to run SWC over these npm packages so
-  // private fields (and other modern syntax) get compiled.
-  transpilePackages: [
-    "firebase",
-    "undici"
-  ],
-
+module.exports = withTM({
   webpack(config) {
-    // your existing alias…
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
-};
+});
