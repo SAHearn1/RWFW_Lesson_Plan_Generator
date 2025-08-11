8f9bae15faa4dfa455c95df54486811e677d1644
SAHearn1 <Hearn.sa@gmail.com>
2025-08-11 14:12:34 -0400

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,11 @@
 // next.config.cjs
-const path = require('path');
+const path = require("path");
 
+/** @type {import('next').NextConfig} */
 module.exports = {
+  eslint: { ignoreDuringBuilds: true }, // <-- optional safety
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
 };
