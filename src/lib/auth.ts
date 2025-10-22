aad4d9a4f6d840e563cafa816551bab25feebc81
SAHearn1 <Hearn.sa@gmail.com>
2025-10-21 20:55:08 -0400

    Add import for PrismaAdapter in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import type { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
 import { prisma } from '@/lib/db';
 
 const googleClientId = process.env.GOOGLE_CLIENT_ID;
